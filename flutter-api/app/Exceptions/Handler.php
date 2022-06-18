cb535643ed2e6e2649dbea948e505049eddd2b70
Mohammadmohebi33 <mohammad.rasoul.mohebi@gmail.com>
2022-06-18 21:25:52 +0430

    error handling

--- flutter-api/app/Exceptions/Handler.php
+++ flutter-api/app/Exceptions/Handler.php
@@ -34,8 +34,8 @@ class Handler extends ExceptionHandler
      */
     public function register()
     {
-        $this->reportable(function (Throwable $e) {
-            //
+        $this->renderable(function (Throwable $e) {
+            return response()->json(['message' => 'Object not found'] , 404);
         });
     }
 }
--- flutter-api/routes/api.php
+++ flutter-api/routes/api.php
@@ -19,7 +19,5 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-//Route::get('categories' , [CategoryController::class , 'index']);
-//Route::get('categories/{category}' , [CategoryController::class , 'show']);
 
-Route::resource('categories', \App\Http\Controllers\Api\CategoryController::class);
+Route::apiResource('categories', \App\Http\Controllers\Api\CategoryController::class);
