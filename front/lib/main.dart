2cefce8ca0e417c2e7a33e262eb1f5f70bfb2872
Mohammadmohebi33 <mohammad.rasoul.mohebi@gmail.com>
2022-06-19 18:44:54 +0430

    login page

--- front/lib/main.dart
+++ front/lib/main.dart
@@ -16,26 +16,42 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: const Text('login'),
         ),
-        body: Column(
-          children:   <Widget>[
-           const TextField(
-              keyboardType: TextInputType.emailAddress,
-              decoration: InputDecoration(
-                labelText: 'login',
+        body: Container(
+          color: Theme.of(context).primaryColorDark,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Card(
+                elevation: 8,
+                margin: EdgeInsets.only(left: 16 , right: 16),
+               color: Colors.white,
+                child: Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Column(
+                    children:   <Widget>[
+                     const TextField(
+                        keyboardType: TextInputType.emailAddress,
+                        decoration: InputDecoration(
+                          labelText: 'login',
+                        ),
+                      ),
+                      const  TextField(
+                        keyboardType: TextInputType.visiblePassword,
+                        decoration: InputDecoration(
+                          labelText: 'password',
+                        ),
+                      ),
+                      ElevatedButton(
+                        onPressed: (() {
+                          print("hello login");
+                        }),
+                         child: Text('login') , style: ElevatedButton.styleFrom(minimumSize: Size(double.infinity, 36)),)
+                    ],
+                  ),
+                ),
               ),
-            ),
-            const  TextField(
-              keyboardType: TextInputType.visiblePassword,
-              decoration: InputDecoration(
-                labelText: 'password',
-              ),
-            ),
-            ElevatedButton(
-              onPressed: (() {
-                print("hello login");
-              }),
-               child: Text('login') , style: ElevatedButton.styleFrom(minimumSize: Size(double.infinity, 36)),)
-          ],
+            ],
+          ),
         ),
       ),
     );
