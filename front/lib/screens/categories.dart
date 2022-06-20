38811ef4e8a9469811f735877e43c570fa5035bd
Mohammadmohebi33 <mohammad.rasoul.mohebi@gmail.com>
2022-06-20 13:01:41 +0430

    get categories from back

--- front/lib/screens/categories.dart
+++ front/lib/screens/categories.dart
@@ -1,47 +1,76 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert' as convert;
 
 
+class Category{
+
+  int id ; 
+  String name; 
+  Category({required this.id ,required this.name});
+
+  factory Category.formJson(Map<String , dynamic>json){
+    return Category(id: json['id'], name: json['name']);
+  }
+
+
+}
+
+
+ 
 class Categories extends StatefulWidget {
-  const Categories({Key? key}) : super(key: key);
 
   @override
   State<Categories> createState() => _CategoriesState();
 }
 
 class _CategoriesState extends State<Categories> {
+ 
+ late Future<List<Category>> futureCategory ; 
+  
+  Future<List<Category>> fetchcategory() async {
+    http.Response response = await http.get(Uri.parse('http://192.168.1.9:8000/api/categories'));
+    List categories = convert.jsonDecode(response.body);
+    return categories.map((categories) => Category.formJson(categories)).toList();
+  }
+  
 
-  final List<String> categories = <String>[
-      'Category 1',
-      'Category 2',
-      'Category 3',
-  ];
-
-  int clicked = 0 ;
+  @override
+  void initState() {
+    super.initState();
+    futureCategory = fetchcategory(); 
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('categories clicked : $clicked'),
+        title: Text('categories'),
       ),
       body: Container(
         color: Theme.of(context).primaryColorDark,
         child: Center(
-          child: ListView.builder(
-            itemCount: categories.length,
-            itemBuilder: (BuildContext context , int index){
-              return ListTile(
-                title: Text(categories[index] , style: const TextStyle(color: Colors.white),),
-                onTap: () {
-                  setState(() {
-                    clicked++; 
-                  });
-                }
+         child: FutureBuilder<List<Category>>(
+          future: futureCategory,
+          builder: (context,snapshot){
+            if(snapshot.hasData){
+              return ListView.builder(
+                 itemCount: snapshot.data!.length,
+                 itemBuilder: (context , index){
+                  Category c = snapshot.data![index];
+                  return ListTile(
+                    title: Text(c.name),
+                  );
+                 },
               );
             }
-          ),
+            return CircularProgressIndicator(); 
+          }
+         ),
         ),
       ),
     );
   }
 }
+
+
